a75b5f79b2a31342a0c034d30a25697c3423b2b7
Aman KUmar <amankumar.spj410@gmail.com>
2023-06-17 16:39:44 +0530

    new design

---
+++ app.js
@@ -0,0 +1,14 @@
+// 5 data types of js?
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Object
+
+console.log("Hello World");
+console.log(123.3242348734);
+console.log(true);
+console.log(undefined);
+console.log(null);
+console.log({ name: "John" });
